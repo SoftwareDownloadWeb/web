91808fad2e1537ff21c819e48797318c5b173a03
1561656363 <43992277+1561656363@users.noreply.github.com>
2020-12-14 22:11:25 +0800

    更新登录注册样式

--- assets/js/login/Suspended_window.js
+++ assets/js/login/Suspended_window.js
@@ -9,3 +9,19 @@ function login_test(id)
 function login_cancelBubble(){
     event.cancelBubble=true;
 }
+
+function login(type) {
+    var iframe= document.getElementById("a_iframe");
+    var a = document.getElementById("a_login");
+    var b = document.getElementById("a_register");
+    if(type=="login"){
+        a.style.color="black";
+        b.style.color="gray";
+    }
+    else{
+        b.style.color="black";
+        a.style.color="gray";
+    }
+    iframe.src = "login/" + type + ".html";
+    console.log(iframe.src);
+}
