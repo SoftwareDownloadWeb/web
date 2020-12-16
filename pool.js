1c3457941bbcbef05a1b07e27482ec09e4002a82
1561656363 <43992277+1561656363@users.noreply.github.com>
2020-12-16 19:24:33 +0800

    覆盖

--- pool.js
+++ pool.js
@@ -5,11 +5,7 @@ const pool=mysql.createPool({
     host:'127.0.0.1',
     port:'3306',
     user:'root',
-<<<<<<< HEAD
-    password:'20150324',
-=======
-    password:'87654321',
->>>>>>> 473bf861dd4bbcbb695ef4bd673990a9038d7b69
+    password:'123456',
     database:'myweb',
     connectionLimit:'20'
 })
