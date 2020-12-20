e97d90280b9b0ad2e64a2ec7662fdcd045c54072
ajiur <45103176+ajiur@users.noreply.github.com>
2020-12-20 11:38:59 +0800

    更新登陆注册部分

--- router/user.js
+++ router/user.js
@@ -1,36 +1,48 @@
-//处理用户登录注册的路由
 const express=require('express');
 const path=require('path');
-
 // 引入连接池的模块
 const pool=require('../pool.js');
 //创建路由器对象
 
 const r=express.Router();
 
-
-//中间处理各种数据
+//登录
 r.post('/login',(req,res)=>{
-   console.log(req.body);
    let username=req.body.username;
    let password=req.body.password;
    pool.query("SELECT name FROM user WHERE name= ? AND password = ?",[username,password],function (err,data){
       if(err) throw err;
+      res.cookie('mystatus','log',{path:'/',maxAge:60*1000*200});
       if (data.length!==0) {
-         console.log(data);
          res.cookie('username',data[0].name,{path:'/',maxAge:60*1000*200});
          res.end();
       }else{
-         console.log("error");
          res.cookie('username','error',{path:'/',maxAge:60*1000*200});
          res.end();
       }
 
    })
-  // res.sendFile(path.join(__dirname,'../public/login.html'));
-   // res.redirect('http://baidu.com');
-   // res.send("h?");
 
+})
+//注册
+r.post('/register',(req,res)=>{
+   let username=req.body.username;
+   let email=req.body.email;
+   let password=req.body.password;
+   pool.query("SELECT * FROM user WHERE email = ?",[email],function (err,data){
+      if(err) throw err;
+      if (data.length!==0){
+         res.cookie('mystatus','error',{path:'/',maxAge:60*1000*200});
+         res.end();
+      }
+      else{
+         pool.query('INSERT INTO user (name,password,email) VALUES (?,?,?)',[username,password,email],function (err,data){
+            if(err) throw err;
+            res.cookie('mystatus','success',{path:'/',maxAge:60*1000*200});
+            res.end();
+         });
+      }
+   })
 })
 
 
